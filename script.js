e2852aac69d5d108f91d16169d8846c8d57284f6
MLG-Craft <awanammar122@gmail.com>
2022-09-08 20:35:34 +0500

    Alot

---
+++ script.js
@@ -0,0 +1,12 @@
+function Info() {
+    window.location.href = "Info.html";
+}
+document.getElementById("Info").onclick = Info;
+function Settings() {
+    window.location.href = "Settings.html";
+}
+document.getElementById("Settings").onclick = Settings;
+function Search() {
+    window.location.href = "Search.html";
+}
+document.getElementById("Search").onclick = Search;
