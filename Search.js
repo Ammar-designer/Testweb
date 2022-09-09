659f79ff066f19a75010bd10baadc601e38ca9f3
MLG-Craft <awanammar122@gmail.com>
2022-09-09 17:12:15 +0500

    Update Search.js

--- Search.js
+++ Search.js
@@ -1,11 +1,12 @@
-var InputValue = document.getElementById("search").value;
 function steamunlocked()
 {
+    var InputValue = document.getElementById("search").value;
     window.open("https://steamunlocked.net/?s=" + InputValue);
 }
 
 function crotorrent()
 {
+    var InputValue = document.getElementById("search").value;
     window.open("https://crotorrents.com/?s=" + InputValue);
 }
 
