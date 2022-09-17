02f510cef32e8f248d327f010534e10c15451723
Ammar-designer <awanammar122@gmail.com>
2022-09-17 10:45:23 +0500

    f

--- Search.js
+++ Search.js
@@ -12,3 +12,14 @@ function crotorrent()
 
 document.getElementById("Steamunlocked").onclick = steamunlocked;
 document.getElementById("Crotorrent").onclick = crotorrent;
+
+console.log("Type custom.help() to Check Every Command In This Console");
+
+const custom = {
+    help() {
+        console.log("custom.clear()");
+    },
+    clear() {
+        console.clear()
+    },
+};
